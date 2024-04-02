df61cb3adebd627984dd6f311743b33baa6088b9
Nadezhda <nadezhdadieslast@gmail.com>
2024-04-02 10:49:43 +0400

    create .gitignore, input the vector size, filled the vector

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> vector;
+    int size;
+    int numbers;
+    int numberToDelete;
+
+    std::cout << "Input vector size: ";
+    std::cin >> size;
+    vector.resize(size);
+
+    std::cout << "Input numbers: ";
+    for (int i = 0; i < size; i++){
+        std::cin >> numbers;
+        vector[i] = numbers;
+    }
+
+    for (int i = 0; i < size; i++){
+        std::cout << vector[i] << " ";
+    }
+
+
+}
